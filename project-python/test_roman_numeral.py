d6fe3f6b73230b6e32855020443f33f3601f452e
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-01-23 14:19:52 +0000

    pytest

--- project-python/test_roman_numeral.py
+++ project-python/test_roman_numeral.py
@@ -1,23 +1,13 @@
 from roman_number import to_arabic_number
 from roman_number import to_roman_numeral
+import pytest
 
-#def test_to_arabic_number(rom,result):
- #   assert to_arabic_number(rom) == result
-@pytest.fixture
-def num():
-    num = 
+@pytest.mark.parametrize("rom, result", [('I',1), ('MMVIII',2008), ('IV',4), ('XC',90), ('MMMCMXCIX',3999)])
+def test_to_arabic_number(rom,result):
+   assert to_arabic_number(rom) == result
+
+@pytest.mark.parametrize("num, result", [(1,'I'), (2008,'MMVIII'), (4,'IV'), (90,'XC'), (3999,'MMMCMXCIX')]) 
 def test_to_roman_numeral(num, result):
     assert to_roman_numeral(num) == result
 
-to_test = {1:'I', 2008:'MMVIII', 4:'IV', 90:'XC', 3999:'MMMCMXCIX'}
-for item in to_test.keys():
-    print(item, to_test[item])
-    test_to_roman_numeral(item, to_test[item])
 
-'''
-to_test2 = {'I':1, 'MMVIII':2008, 'IV':4, 'XC':90, 'MMMCMXCIX':3999}
-for item in to_test2.keys():
-    rom = item
-    result = to_test2[rom]
-    test_to_arabic_number(rom, result)
-'''
